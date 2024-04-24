657e2aed64e515975920abf758af52cbd1dfb1c6
cjrcrc443 <168005081+cjrcrc443@users.noreply.github.com>
2024-04-24 18:38:37 +0300

    Add files via upload

---
+++ AlinaXIQ/plugins/admins/pause.py
@@ -0,0 +1,22 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from strings.filters import command
+from AlinaXIQ import app
+from AlinaXIQ.core.call import Alina
+from AlinaXIQ.utils.database import is_music_playing, music_off
+from AlinaXIQ.utils.decorators import AdminRightsCheck
+from AlinaXIQ.utils.inline import close_markup
+from config import BANNED_USERS
+
+
+@app.on_message(command(["pause", "cpause","/pause","/cpause","وسبە","وەستانی کاتی"]) & ~filters.private & ~BANNED_USERS)
+@AdminRightsCheck
+async def pause_admin(cli, message: Message, _, chat_id):
+    if not await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_1"])
+    await music_off(chat_id)
+    user_mention = message.from_user.mention if message.from_user else "𝖠𝖽𝗆𝗂𝗇"
+    await Alina.pause_stream(chat_id)
+    await message.reply_text(
+        _["admin_2"].format(user_mention), reply_markup=close_markup(_)
+    )
---
+++ AlinaXIQ/plugins/admins/resume.py
@@ -0,0 +1,22 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from strings.filters import command
+from AlinaXIQ import app
+from AlinaXIQ.core.call import Alina
+from AlinaXIQ.utils.database import is_music_playing, music_on
+from AlinaXIQ.utils.decorators import AdminRightsCheck
+from AlinaXIQ.utils.inline import close_markup
+from config import BANNED_USERS
+
+
+@app.on_message(command(["resume", "cresume","/resume","/cresume","د","دەستپێکردنەوە"]) & ~filters.private & ~BANNED_USERS)
+@AdminRightsCheck
+async def resume_com(cli, message: Message, _, chat_id):
+    if await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_3"])
+    await music_on(chat_id)
+    user_mention = message.from_user.mention if message.from_user else "𝖠𝖽𝗆𝗂𝗇"
+    await Alina.resume_stream(chat_id)
+    await message.reply_text(
+        _["admin_4"].format(user_mention), reply_markup=close_markup(_)
+    )
