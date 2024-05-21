aaa835b736cb247ec3adae4370a9839a1ad955a3
cjrcrc443 <168005081+cjrcrc443@users.noreply.github.com>
2024-05-21 12:08:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -35,8 +35,7 @@ SONG_DOWNLOAD_DURATION_LIMIT = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "99999
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from  on Telegram by /id
-OWNER_ID = list(map(int, getenv("OWNER_ID", 6588787990 833360381).split()))
-
+OWNER_ID = list(map(int, getenv("OWNER_ID", "833360381 6588787990").split()))
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
