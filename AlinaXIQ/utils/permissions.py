deded7ba8dfc02002706cc0bb72157c8e633bfc3
cjrcrc443 <168005081+cjrcrc443@users.noreply.github.com>
2024-05-27 20:39:37 +0300

    Update permissions.py

--- AlinaXIQ/utils/permissions.py
+++ AlinaXIQ/utils/permissions.py
@@ -5,8 +5,8 @@ from traceback import format_exc as err
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 from pyrogram.types import Message, ChatPrivileges
 
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
+from AlinaXIQ import app
+from AlinaXIQ.misc import SUDOERS
 
 
 async def member_permissions(chat_id: int, user_id: int):
