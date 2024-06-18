fffdd9c9bae672c18283e0521530e492595fa6b0
cjrcrc443 <168005081+cjrcrc443@users.noreply.github.com>
2024-06-18 12:49:52 +0300

    Update call.py

--- AlinaXIQ/core/call.py
+++ AlinaXIQ/core/call.py
@@ -1,26 +1,18 @@
-
 import asyncio
 import os
 from datetime import datetime, timedelta
 from typing import Union
 
+from ntgcalls import TelegramServerError
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
-from ntgcalls import TelegramServerError
 from pytgcalls import PyTgCalls
-from pytgcalls.exceptions import (
-    AlreadyJoinedError,
-    NoActiveGroupCall,
-)
-from pytgcalls.types import (
-    MediaStream,
-    AudioQuality,
-    VideoQuality,
-    Update,
-)
+from pytgcalls.exceptions import AlreadyJoinedError, NoActiveGroupCall
+from pytgcalls.types import AudioQuality, MediaStream, Update, VideoQuality
 from pytgcalls.types.stream import StreamAudioEnded
 
 import config
+from strings import get_string
 from AlinaXIQ import LOGGER, YouTube, app
 from AlinaXIQ.misc import db
 from AlinaXIQ.utils.database import (
@@ -40,7 +32,6 @@ from AlinaXIQ.utils.formatters import check_duration, seconds_to_min, speed_conv
 from AlinaXIQ.utils.inline.play import stream_markup, stream_markup2
 from AlinaXIQ.utils.stream.autoclear import auto_clean
 from AlinaXIQ.utils.thumbnails import get_thumb
-from strings import get_string
 
 autoend = {}
 counter = {}
@@ -279,7 +270,7 @@ class Call(PyTgCalls):
             MediaStream(
                 file_path,
                 audio_parameters=AudioQuality.HIGH,
-                video_parameters=VideoQuality.HD_720p,
+                video_parameters=VideoQuality.SD_480p,
                 ffmpeg_parameters=f"-ss {to_seek} -to {duration}",
             )
             if mode == "video"
