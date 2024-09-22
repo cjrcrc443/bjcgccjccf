2898ff0248ba4f6c9dadaad8821b2bc3a1cc9df5
cjrcrc443 <168005081+cjrcrc443@users.noreply.github.com>
2024-09-22 21:21:14 +0300

    Update logging.py

--- AlinaXIQ/logging.py
+++ AlinaXIQ/logging.py
@@ -1,5 +1,11 @@
 import logging
 
+
+class SuppressBSONFilter(logging.Filter):
+    def filter(self, record):
+        return "bson.errors.InvalidDocument" not in record.getMessage()
+
+
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
@@ -10,11 +16,18 @@ logging.basicConfig(
     ],
 )
 
+logger = logging.getLogger()
+logger.addFilter(SuppressBSONFilter())
+
 logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
 logging.getLogger("pymongo").setLevel(logging.ERROR)
 
+# Setting ntgcalls logger level and disabling propagation
+ntgcalls_logger = logging.getLogger("ntgcalls")
+ntgcalls_logger.setLevel(logging.CRITICAL)
+ntgcalls_logger.propagate = False
 
 
 def LOGGER(name: str) -> logging.Logger:
