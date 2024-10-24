d9ae28253ccc77e10c253065423fcb5f306c01a8
cjrcrc443 <168005081+cjrcrc443@users.noreply.github.com>
2024-10-24 21:22:58 +0300

    Update database.py

--- AlinaXIQ/utils/database.py
+++ AlinaXIQ/utils/database.py
@@ -10,7 +10,7 @@ from AlinaXIQ.core.mongo import mongodb, pymongodb
 authdb = mongodb.adminauth
 authuserdb = mongodb.authuser
 autoenddb = mongodb.autoend
-db = mongodb.assistants
+assdb = mongodb.assistants
 blacklist_chatdb = mongodb.blacklistChat
 blockeddb = mongodb.blockedusers
 chatsdb = mongodb.chats
@@ -60,44 +60,43 @@ video = {}
 assistantdict = {}
 
 
+async def get_assistant_number(chat_id: int) -> str:
+    assistant = assistantdict.get(chat_id)
+    return assistant
+
+
 async def get_client(assistant: int):
-    clients = userbot.clients
-    if 1 <= assistant <= len(userbot.clients):
-        return clients[assistant - 1]
-    return None
+    if int(assistant) == 1:
+        return userbot.one
+    elif int(assistant) == 2:
+        return userbot.two
+    elif int(assistant) == 3:
+        return userbot.three
+    elif int(assistant) == 4:
+        return userbot.four
+    elif int(assistant) == 5:
+        return userbot.five
 
 
-async def save_assistant(chat_id, number):
+async def set_assistant_new(chat_id, number):
     number = int(number)
-    assistantdict[chat_id] = number
-    await db.update_one(
+    await assdb.update_one(
         {"chat_id": chat_id},
         {"$set": {"assistant": number}},
         upsert=True,
     )
-    return await get_assistant(chat_id)
 
 
 async def set_assistant(chat_id):
     from AlinaXIQ.core.userbot import assistants
 
-    dbassistant = await db.find_one({"chat_id": chat_id})
-    current_assistant = dbassistant["assistant"] if dbassistant else None
-
-    available_assistants = [assi for assi in assistants if assi != current_assistant]
-
-    if len(available_assistants) <= 1:
-        ran_assistant = random.choice(assistants)
-    else:
-        ran_assistant = random.choice(available_assistants)
-
+    ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
-    await db.update_one(
+    await assdb.update_one(
         {"chat_id": chat_id},
         {"$set": {"assistant": ran_assistant}},
         upsert=True,
     )
-
     userbot = await get_client(ran_assistant)
     return userbot
 
@@ -107,7 +106,7 @@ async def get_assistant(chat_id: int) -> str:
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
-        dbassistant = await db.find_one({"chat_id": chat_id})
+        dbassistant = await assdb.find_one({"chat_id": chat_id})
         if not dbassistant:
             userbot = await set_assistant(chat_id)
             return userbot
@@ -134,7 +133,7 @@ async def set_calls_assistant(chat_id):
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
-    await db.update_one(
+    await assdb.update_one(
         {"chat_id": chat_id},
         {"$set": {"assistant": ran_assistant}},
         upsert=True,
@@ -142,18 +141,19 @@ async def set_calls_assistant(chat_id):
     return ran_assistant
 
 
-async def group_assistant(self, chat_id: int) -> PyTgCalls:
+async def group_assistant(self, chat_id: int) -> int:
     from AlinaXIQ.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
-        dbassistant = await db.find_one({"chat_id": chat_id})
+        dbassistant = await assdb.find_one({"chat_id": chat_id})
         if not dbassistant:
             assis = await set_calls_assistant(chat_id)
         else:
             assis = dbassistant["assistant"]
             if assis in assistants:
                 assistantdict[chat_id] = assis
+                assis = assis
             else:
                 assis = await set_calls_assistant(chat_id)
     else:
@@ -161,13 +161,16 @@ async def group_assistant(self, chat_id: int) -> PyTgCalls:
             assis = assistant
         else:
             assis = await set_calls_assistant(chat_id)
-
-    assistant_index = int(assis) - 1
-
-    if 0 <= assistant_index < len(self.calls):
-        return self.calls[assistant_index]
-    else:
-        raise ValueError(f"Assistant index {assistant_index + 1} is out of range.")
+    if int(assis) == 1:
+        return self.one
+    elif int(assis) == 2:
+        return self.two
+    elif int(assis) == 3:
+        return self.three
+    elif int(assis) == 4:
+        return self.four
+    elif int(assis) == 5:
+        return self.five
 
 
 async def get_notes_count() -> dict:
