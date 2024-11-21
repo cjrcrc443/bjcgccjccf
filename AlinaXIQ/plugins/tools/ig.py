8be52c46749f04c8490dc33fc815a12e6cecc008
cjrcrc443 <168005081+cjrcrc443@users.noreply.github.com>
2024-11-21 19:39:18 +0300

    Create ig.py

---
+++ AlinaXIQ/plugins/tools/ig.py
@@ -0,0 +1,52 @@
+import re
+
+import requests
+from AlinaXIQ import app
+from config import SUPPORT_CHANNEL
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+# Regex pattern to match Instagram URLs
+instagram_url_pattern = r"(https?://(?:www\.)?instagram\.com/[-a-zA-Z0-9@:%._\+~#=]{2,256}/[-a-zA-Z0-9@:%._\+~#=]+)"
+
+
+@app.on_message(filters.regex(instagram_url_pattern))
+async def download_instagram_video(app, message):
+    try:
+        # Extract the Instagram link from the message
+        link = re.search(instagram_url_pattern, message.text).group(1)
+
+        # Make a request to fetch the video
+        json_data = {"url": link}
+        response = requests.post(
+            "https://insta.savetube.me/downloadPostVideo", json=json_data
+        ).json()
+
+        # Extract video and thumbnail
+        thu = response["post_video_thumbnail"]
+        video = response["post_video_url"]
+
+        # Send thumbnail as a photo
+        await message.reply_text("**← کەمێک چاوەڕێ بکە .. ڤیدیۆ دادەبەزێت ...**")
+
+        # Send video directly
+        caption = "**✅ ꒐ بە سەرکەوتوویی داگرترا\n🎸 ꒐ @IQMCBOT**"
+        await app.send_video(
+            message.chat.id,
+            video,
+            caption=caption,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text=_["S_B_6"],
+                            url=f"{SUPPORT_CHANNEL}",
+                        )
+                    ]
+                ]
+            ),
+        )
+
+    except Exception as e:
+        print(f"Error: {e}")
+        await message.reply("**ببورە، هەڵەیەک ڕوویدا! تکایە دوبارە هەوڵدەوە.**")
