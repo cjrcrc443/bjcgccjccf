6073e9c7cdd15750c715da858a78230dcf906479
cjrcrc443 <168005081+cjrcrc443@users.noreply.github.com>
2024-11-21 19:55:49 +0300

    Update ig.py

--- AlinaXIQ/plugins/tools/ig.py
+++ AlinaXIQ/plugins/tools/ig.py
@@ -4,43 +4,32 @@ import requests
 from AlinaXIQ import app
 import requests
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton as Btn, InlineKeyboardMarkup as Mak
 
 # Regex pattern to match Instagram URLs
 instagram_url_pattern = r"(https?://(?:www\.)?instagram\.com/[-a-zA-Z0-9@:%._\+~#=]{2,256}/[-a-zA-Z0-9@:%._\+~#=]+)"
 
-
-# To store sent video message URLs
-sent_video_messages = {}
-
-
-@app.on_message(filters.regex(instagram_url_pattern))
+@app.on_message(filters.text & ~filters.private)
 async def down(client, message):
     try:
         link = message.text
         json_data = {'url': link}
         response = requests.post('https://insta.savetube.me/downloadPostVideo', json=json_data).json()
+
+        # Extract video and thumbnail
         thu = response['post_video_thumbnail']
         video = response['post_video_url']
 
-        # Sending the thumbnail with a button
-        sent_message = await message.reply_photo(
-            thu,
-            reply_markup=Mak([[Btn("داگرتنی ڤیدیۆ", callback_data=f"vid:{message.message_id}")]])
-        )
-        sent_video_messages[sent_message.message_id] = video
+        # Send thumbnail as a photo
+        await message.reply_photo(thu, caption="*ڤیدیۆکە دابەزاندنی دەستپێدەکات...*\n\n⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝙄𝙌 - @MGIMT")
 
-    except Exception:
-        await message.reply("**لینك هەڵەیە ئەزیزم**")
-
-
-@app.on_callback_query(filters.regex(r"^vid:(\d+)$"))
-async def all(client, callback_query):
-    message_id = int(callback_query.data.split(":")[1])
-    if message_id in sent_video_messages:
-        video = sent_video_messages.pop(message_id)
-        await callback_query.message.delete()
+        # Send video directly
         caption = (
-            "**✅ ꒐ بە سەرکەوتوویی داگرترا\n🎸 ꒐ @IQMCBOT**"
+            "*بە سەرکەوتوویی داگرترا لەلایەن :\n"
+            "⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝙄𝙌 - @MGIMT\n\n"
+            "@xv7amo - جۆینی ئەم کەناڵە شازە بکە♥️⚡️*"
         )
-        await app.send_video(callback_query.message.chat.id, video, caption=caption)
+        await app.send_video(message.chat.id, video, caption=caption)
+
+    except Exception as e:
+        print(f"Error: {e}")
+        await message.reply("**لینك هەڵەیە ئەزیزم**")
